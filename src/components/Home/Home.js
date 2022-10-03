e977ceffe0b7d3ddff7633eb74a6a392006ffd7f
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-03 21:47:19 +0600

    home page done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import img from "../../images/giphy.gif";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col gap-5 p-5">
+      <h1 className="text-5xl text-center font-medium">
+        Welcome to Ema John Shop
+      </h1>
+      <img
+        src={img}
+        alt="a boy showing thumb gif"
+        className="m-auto object-scale-down"
+      />
     </div>
   );
 };
